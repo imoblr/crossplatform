5c246512c26485e96a595e9dbc5940d2d581b21d
Kaue Machado <kaumac@gmail.com>
2024-10-02 01:47:42 -0300

    wip base

---
+++ context/ThemeContext.tsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+type ThemeContextType = {
+    colorMode?: "dark" | "light";
+    toggleColorMode?: () => void;
+  };
+
+  export const ThemeContext = createContext<ThemeContextType>({
+    colorMode: "light",
+    toggleColorMode: () => {},
+  });
