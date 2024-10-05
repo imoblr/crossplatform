7fecbc6b00c4add960ed7c5342c8de805f02327c
Kaue Machado <kaumac@gmail.com>
2024-10-05 20:06:40 -0300

    wip theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,16 +21,16 @@ module.exports = {
       'xxl': '1.4rem', 
       'full': '9999px',
     },
-    boxShadow: {
-      'xs': '0 1px 2px 0 rgba(16, 24, 40, 0.05)',
-      'sm': '0 1px 2px 0 rgba(16, 24, 40, 0.06), 0 1px 3px 0 rgba(16, 24, 40, 0.1)',
-      'md': '0 2px 4px -2px rgba(16, 24, 40, 0.06), 0 4px 8px -2px rgba(16, 24, 40, 0.1)',
-      'lg': '0 4px 6px -2px rgba(16, 24, 40, 0.03), 0 12px 16px -4px rgba(16, 24, 40, 0.08)',
-      'xl': '0 8px 8px -4px rgba(16, 24, 40, 0.03), 0 20px 24px -4px rgba(16, 24, 40, 0.08)',
-      '2xl': '0 24px 48px -12px rgba(16, 24, 40, 0.18)',
-      '3xl': '0 32px 64px -12px rgba(16, 24, 40, 0.14)',
-    },
     extend: {
+      boxShadow: {
+        'xs': '0 1px 2px 0 rgba(16, 24, 40, 0.05)',
+        'sm': '0 1px 2px 0 rgba(16, 24, 40, 0.06), 0 1px 3px 0 rgba(16, 24, 40, 0.1)',
+        'md': '0 2px 4px -2px rgba(16, 24, 40, 0.06), 0 4px 8px -2px rgba(16, 24, 40, 0.1)',
+        'lg': '0 4px 6px -2px rgba(16, 24, 40, 0.03), 0 12px 16px -4px rgba(16, 24, 40, 0.08)',
+        'xl': '0 8px 8px -4px rgba(16, 24, 40, 0.03), 0 20px 24px -4px rgba(16, 24, 40, 0.08)',
+        '2xl': '0 24px 48px -12px rgba(16, 24, 40, 0.18)',
+        '3xl': '0 32px 64px -12px rgba(16, 24, 40, 0.14)',
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'var(--color-input)', 
