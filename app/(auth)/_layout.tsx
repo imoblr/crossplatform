e6e06ceaed41e53701dc060e0a7c4832e902eb59
Kaue Machado <kaumac@gmail.com>
2024-10-10 01:50:31 -0300

    wip login

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Text } from "@/components/ui";
 import { Slot } from "expo-router";
+import { Image } from "react-native";
 import { useRef, useState } from "react";
 import { Dimensions, Pressable, StyleSheet } from "react-native";
 import { useWindowDimensions, View } from "react-native";
@@ -12,7 +13,18 @@ import Carousel, {
 const TestimonialImage1 = require("../../assets/images/testimonial-henrico.jpg");
 const TestimonialImage2 = require("../../assets/images/testimonial-monica.jpg");
 
-const data = [...new Array(6).keys()];
+const testimonials = [
+	{
+		name: "Henrico Chagas",
+		image: TestimonialImage1,
+		text: "A Imoblr nos ajudou a otimizar nossas oportunidades de negócio e diminuiu nossos custos operacionais.",
+	},
+	{
+		name: "Monica Oliveira",
+		image: TestimonialImage2,
+		text: "Conseguimos colocar nossos processos repetitivos no piloto automático e economizamos muito tempo!",
+	},
+];
 const width = 520;
 
 export default function AuthLayout() {
@@ -32,42 +44,38 @@ export default function AuthLayout() {
 
 	return (
 		<View className="flex-1 flex-row bg-background p-16">
-			<Box className="flex flex-1 flex-row justify-around gap-3">
+			<Box className="flex flex-1 flex-row justify-around gap-3 rounded-l-xxl bg-background-darker">
 				<Slot />
 			</Box>
-			<Box className="position-relative w-[520px] flex-row justify-around gap-3 overflow-hidden rounded-xl bg-slate-10">
+			<Box className="position-relative w-[520px] flex-row justify-around gap-3 overflow-hidden rounded-r-xxl bg-slate-10">
 				<Carousel
 					ref={ref}
 					width={width}
 					height="100%"
-					data={data}
+					data={testimonials}
 					onProgressChange={progress}
 					renderItem={({ index }) => (
-						<View
-							style={{
-								flex: 1,
-								borderWidth: 1,
-								justifyContent: "center",
-							}}
-						>
-							<Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
-						</View>
+						<Center className="h-full w-full">
+							<Image
+								source={testimonials[index].image}
+								style={{ height: "100%", minWidth: width }}
+							/>
+						</Center>
 					)}
 				/>
 				<Box className="absolute bottom-0 left-0 z-10 w-full bg-background-darkest-a3 px-8 py-6 bg-blend-overlay backdrop-blur-sm">
 					<Pagination.Basic
 						progress={progress}
-						data={data}
+						data={testimonials}
 						dotStyle={{ backgroundColor: "rgba(0,0,0,0.2)", borderRadius: 50 }}
 						containerStyle={{ gap: 5, marginTop: 10 }}
 						onPress={onPressPagination}
 					/>
 					<Text className="pr-16 font-medium text-[#FFF] text-xl">
-						"A Imoblr nos ajudou a otimizar nossas oportunidades de negócio e
-						diminuiu nossos custos operacionais"
+						{testimonials[progress.value].text}
 					</Text>
 					<Box className="w-full flex-row justify-between gap-2">
-						<Box>sdfsd</Box>
+						<Box>{progress.value}</Box>
 					</Box>
 				</Box>
 			</Box>
