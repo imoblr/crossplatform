f692bbd24586812d4ea18bc3aced564e7745d76e
Kaue Machado <kaumac@gmail.com>
2024-10-13 18:32:03 -0300

    wip theming

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
 					"border shadow-[inset_1px_0_0.4px_0_var(--color-primary-600),inset_0_1px_0.4px_0_var(--color-primary-500),inset_-1px_0_0.4px_0_var(--color-primary-600),inset_0_-1px_0.4px_0_var(--color-primary-700)] border-primary-700 bg-primary web:hover:opacity-90 active:opacity-90",
 				destructive: "bg-destructive web:hover:opacity-90 active:opacity-90",
 				outline:
-					"border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent",
+					"border border-border bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent",
 				secondary: "bg-secondary web:hover:opacity-80 active:opacity-80",
 				ghost:
 					"web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent",
